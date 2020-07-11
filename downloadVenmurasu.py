e083665974c3acd624cff115b8f687998718f5f1
Raghav Venkataraman <catchmeraghav@gmail.com>
2020-07-11 16:29:23 +0530

    referring constants.py

--- downloadVenmurasu.py
+++ downloadVenmurasu.py
@@ -20,68 +20,12 @@ import os
 from collections import OrderedDict
 from bs4 import BeautifulSoup
 
-venmurasuFolder = "/home/raghav/Desktop/venbooks"
-#venmurasuFolder = "C:\\Users\\Rajasimhan\\Desktop\\venbooks"
 
-books = OrderedDict([
-                     ('Mudharkanal', [(2014, 1, 1), (2014, 2, 19)]), 
-                     ('Mazhaipadal', [(2014, 2, 24), (2014, 5, 26)]),
-                     ('Vannakadal', [(2014, 6, 1), (2014, 8, 10)]),
-                     ('Neelam', [(2014, 8, 20), (2014, 9, 26)]),
-                     ('Prayagai', [(2014, 10, 20), (2015, 1, 19)]),
-                     ('VenmugilNagaram', [(2015, 2, 1), (2015, 5, 2)]),
-                     ('Indraneelam', [(2015,6,1), (2015,8,31)]), 
-                     ('Kandeepam', [(2015,9,15), (2015,11,27)]),
-                     ('Veyyon',[(2015,12,20),(2016,3,6)]),
-                     ('PanniruPadaikaLam',[(2016,03,20),(2016,06,22)]),
-                     ('Solvalarkadu',[(2016,07,20),(2016,9,17)]),
-                     ('Kraatham',[(2016,10,20),(2017,1,10)]),
-                     ('Maamalar',[(2017,2,1),(2017,5,6)]),
-                     ('Neerkolam',[(2017,5,25),(2017,8,29)]),
-                     ('Ezhuthazhal', [(2017,9,15),(2017,12,2)]),
-                     ('Kuruthisaral', [(2017,12,17),(2018,3,5)]),
-                     ('Imaikanam', [(2018,3,25),(2018,5,16)]),
-                     ('Sennavengai', [(2018,6,1),(2018,8,21)]),
-                     ('ThisaitherVellam', [(2018,9,10),(2018,11,28)]),
-                     ('Karkadal',[(2018,12,25),(2019,3,22)]),
-                     ('Irutkani',[(2019,4,10),(2019,6,14)]),
-                     ('TheeyinEdai',[(2019,7,1),(2019,8,26)]),
-                     ('Neersudar', [(2019,9,15),(2019,11,15)]),
-                     ('KalitrruYanaiNirai', [(2019,12,1),(2020,2,18)]),
-                     ('Kalporusirunurai', [(2020,3,15),(2020,6,9)]),
-                     ('MudhalaVin', [(2020,7,1),(2020,7,2)]),
-                   ])
-
-
-booksTamilNames = {
-                    'Mudharkanal' : u'முதற்கனல்',
-                    'Mazhaipadal' : u'மழைப்பாடல்',
-                    'Vannakadal' : u'வண்ணக்கடல்',
-                    'Neelam' : u'நீலம்',
-                    'Prayagai' : u'பிரயாகை',
-                    'VenmugilNagaram' : u'வெண்முகில் நகரம்',
-                    'Indraneelam' : u'இந்திரநீலம்',
-                    'Kandeepam' : u'காண்டீபம்',
-                    'Veyyon' : u'வெய்யோன்',
-                    'PanniruPadaikaLam' : u'பன்னிரு படைக்களம்',
-                    'Solvalarkadu' : u'சொல்வளர்காடு',
-                    'Kraatham' : u'கிராதம்',
-                    'Maamalar' : u'மாமலர்',
-                    'Neerkolam' : u'நீர்க்கோலம்',
-                    'Ezhuthazhal' : u'எழுதழல்',
-                    'Kuruthisaral' : u'குருதிச்சாரல்',
-                    'Imaikanam' : u'இமைக்கணம்',
-                    'Sennavengai' : u'செந்நா வேங்கை',
-                    'ThisaitherVellam' : u'திசைதேர் வெள்ளம்',
-                    'Karkadal' : u'கார்கடல்',
-                    'Irutkani' : u'இருட்கனி',
-                    'TheeyinEdai' : u'தீயின் எடை',
-                    'Neersudar' : u'நீர்ச்சுடர்',
-                    'KalitrruYanaiNirai' : u'களிற்றியானை நிரை',
-                    'Kalporusirunurai' : u'கல்பொருசிறுநுரை',
-                    'MudhalaVin' : u'முதலாவிண்',
-                  }                        
-
+from constants import venmurasufolder
+from constants import jeyamohanfolder
+from constants import books
+from constants import booksTamilNames
+venmurasuFolder = venmurasufolder
 
 class downloadVenmurasu(object):
     ''' 
